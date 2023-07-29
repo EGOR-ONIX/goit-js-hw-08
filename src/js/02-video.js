032ec710c132089ec736199a70fd811fa4818e1c
EGOR-ONIX <127556210+EGOR-ONIX@users.noreply.github.com>
2023-07-30 02:49:55 +0300

    final

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const LS_KEY = 'videoplayer-current-time';
+
+player.on('timeupdate', throttle(getCurrentTime, 1000));
+
+player.setCurrentTime(JSON.parse(localStorage.getItem(LS_KEY)) ?? 0);
+
+function getCurrentTime(data) {
+  const currentTime = data.seconds;
+  localStorage.setItem(LS_KEY, JSON.stringify(currentTime));
+}
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,33 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const LS_KEY = 'feedback-form-state';
+
+const dataForm = JSON.parse(localStorage.getItem(LS_KEY)) ?? {};
+const { email, message } = form.elements;
+
+if (dataForm) {
+  email.value = dataForm.email || '';
+  message.value = dataForm.message || '';
+}
+
+form.addEventListener('input', throttle(onInput, 500));
+form.addEventListener('submit', onSubmit);
+
+function onInput() {
+  dataForm.email = email.value;
+  dataForm.message = message.value;
+  localStorage.setItem(LS_KEY, JSON.stringify(dataForm));
+}
+
+function onSubmit(event) {
+  event.preventDefault();
+
+  if (email.value === '' || message.value === '') {
+    return alert('Please fill in all the fields!');
+  }
+
+  console.log(dataForm);
+  localStorage.removeItem(LS_KEY);
+  event.currentTarget.reset();
+}
